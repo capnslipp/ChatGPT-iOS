79b60518f5e2d3e96005adb76d948028d0d51a5b
Slipp Douglas Thompson <git@capnslipp.com>
2022-12-23 03:46:31 -0600

    Initial Commit

---
+++ ChatGPT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatGPT
+//
+//  Created by Cap'n Slipp on 12/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
