5e10aa714c776875508a4d50e6775fb75a6786e7
Slipp Douglas Thompson <git@capnslipp.com>
2023-03-03 17:56:38 -0600

    Created empty ChatGPTTests for GH Actions
    ChatGPTTests doesn't actually test anything; it only exists to allow CI test runner (GH Actions) to verify the project builds successfully.

---
+++ ChatGPTTests/ChatGPTTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChatGPTTests.swift
+//  ChatGPTTests
+//
+//  Created by Cap'n Slipp on 3/3/23.
+//
+
+import XCTest
+
+/// Doesn't actually test anything; only exists to allow CI test runner to verify the project builds successfully.
+final class ChatGPTTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
